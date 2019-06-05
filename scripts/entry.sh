f14bb8819e272a736a57b15d6af41fade419f3da
Anderson Marcelino <anderson@andersonmarcelino.com.br>
2019-06-05 02:04:39 +0000

    Fix entry script

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ -f ~/workspace/.config ]
+if [ ! -d ~/workspace/.config ]
   then
-  else
     mkdir ~/workspace/.config
 fi
 
-if [ -f ~/workspace/.config/ssh ]
+if [ -d ~/workspace/.config/ssh ]
   then
     cp -R ~/workspace/.config/ssh /root/.ssh
     chmod 700 /root/.ssh
     chmod 644 /root/.ssh/id_rsa.pub
     chmod 600 /root/.ssh/id_rsa
-  else
-    echo "SSH config not found"
 fi
 
 exec "$@"
