3e329aa5f361fbceb7d8dbf59070151746ba1b55
Anderson Marcelino <anderson@andersonmarcelino.com.br>
2019-06-05 01:45:24 +0000

    Add fallback to config folder

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 set -e
 
-cp -R ~/workspace/config/ssh /root/.ssh
-chmod 700 /root/.ssh
-chmod 644 /root/.ssh/id_rsa.pub
-chmod 600 /root/.ssh/id_rsa
+if [ -f ~/workspace/.config ]
+  then
+  else
+    mkdir ~/workspace/.config
+fi
+
+if [ -f ~/workspace/.config/ssh ]
+  then
+    cp -R ~/workspace/.config/ssh /root/.ssh
+    chmod 700 /root/.ssh
+    chmod 644 /root/.ssh/id_rsa.pub
+    chmod 600 /root/.ssh/id_rsa
+  else
+    echo "SSH config not found"
+fi
 
 exec "$@"
